9aec375ba809c2f9c09f83a4788eb6de9a0e2d66
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2019-02-12 21:12:16 +0100

    frontend: display list of rooms

--- src/app/routes/Rooms/Rooms.component.tsx
+++ src/app/routes/Rooms/Rooms.component.tsx
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import getList from "./actions/Rooms.get-list.actions";
+import {REDUCER_NAME__ROOMS} from "./Rooms.reducer";
+
 function mapStateToProps(state: any) {
-  return {};
+  const { tableData, loading } = state[REDUCER_NAME__ROOMS];
+  return {
+    loading,
+    tableData
+  };
 }
 
-class Rooms extends Component {
+const mapDispatchToProps = { getList };
+
+interface RoomsProps { getList: Function, tableData: any, loading: boolean }
+class Rooms extends Component<RoomsProps> {
+  async componentDidMount() {
+    await this.props.getList();
+  }
+
   render() {
+    const { tableData, loading } = this.props;
+
     return (
       <div>
         Rooms table goes here
+        <ul>
+          {!loading && tableData.docs && tableData.docs.map((doc: any, i: number) => <li key={i}>{doc.name}</li>)}
+        </ul>
       </div>
     );
   }
@@ -17,4 +36,5 @@ class Rooms extends Component {
 
 export default connect(
   mapStateToProps,
+  mapDispatchToProps
 )(Rooms);
