25c742c9ab18554de7effdb4c7ee04f6462f4b17
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2019-02-12 16:57:07 +0100

    backend: fixed searching

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -5,32 +5,32 @@ exports.health = function (req, res) {
 };
 
 exports.search = function (req, res, next) {
-  const page = req.query.page || 1;
-  const limit = req.query.limit || 10;
+  const page = req.query.page && parseInt(req.query.page) || 1;
+  const limit = req.query.limit && parseInt(req.query.limit) || 10;
   Room.paginate({}, { page, limit }, function(err, result) {
     if (err) return next(err);
-    res.json({ result })
+    res.json(result)
   });
 };
 
 exports.create = function (req, res, next) {
   new Room({ name: req.body.name }).save(function (err) {
     if (err) return next(err);
-    res.json({ message: 'Product Created successfully' });
+    res.json({ message: 'Room created!' });
   })
 };
 
 exports.details = function (req, res, next) {
   Room.findById(req.params.id, function (err, item) {
     if (err) return next(err);
-    res.json({ item });
+    res.json(item);
   })
 };
 
 exports.update = function (req, res, next) {
-  Room.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+  Room.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err) {
     if (err) return next(err);
-    res.json({ message: 'Product udpated.' });
+    res.json({ message: 'Room updated!' });
   });
 };
 
--- server/routes/room.route.js
+++ server/routes/room.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const roomController = require('../controllers/room.controller');
 
 router.get('/health', roomController.health);
+router.get('/search', roomController.search);
 router.post('/create', roomController.create);
 router.get('/:id', roomController.details);
 router.put('/:id', roomController.update);
